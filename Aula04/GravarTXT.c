a5277cdcdcf55629b060be9627dbc7e5d348cb29
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-02-10 23:07:38 -0300

    Create GravarTXT.c

---
+++ Aula04/GravarTXT.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void FuncVaziaApr(){
+    puts("#----------------------------------------------#");
+    puts("");
+    puts("         AULA FINAL DA INTRODUÇÃO AO C          ");
+    puts("");
+    puts("#----------------------------------------------#");
+}
+
+const char* print(char texto[]){
+    printf("%s\n", texto);
+}
+
+void GravarTXT(FILE Arq, char* Texto){
+    fprintf(Arq, Texto);
+    fclose(Arq);
+}
+
+int main(){
+    
+    FuncVaziaApr();
+    
+    FILE *Arquivo = fopen("Arquivo.txt", "w");
+    
+    if(Arquivo==NULL){
+        print("Não foi possivel localizar o arquivo!");
+        exit(1);
+    }
+    
+    GravarTXT(arquivo, "Meu Primeiro TXT in C. heh");
+    
+}
