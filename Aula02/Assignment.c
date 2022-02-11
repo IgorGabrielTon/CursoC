4bc3f68566ac085bb795568866a59dec1d005b9f
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-02-10 21:41:22 -0300

    Create Assignment.c

---
+++ Aula02/Assignment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+ 
+    int A = 20;
+
+    A += 50; //First ADD
+    A -= 2; //Frist Sub
+    A *= 2; //First Mult
+    A /= 2; //Firts Sub
+    A %= 2; //Firts MOD
+    A <<= 1; //First Bitwise Left
+    A >>= 1; //First Bitwise Right
+    A &= 1; //First Bitwise AND
+    A |= 1; //First Bitwise OR
+    A ^= 1; //Firts Bitwise XOR
+    
+    return 0;
+}
