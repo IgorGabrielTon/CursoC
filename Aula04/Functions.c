5389259273315389295875b03568ff0299f3c999
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-02-10 23:06:35 -0300

    Create Functions.c

---
+++ Aula04/Functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void FuncVaziaApr(){
+    puts("#----------------------------------------------#");
+    puts("");
+    puts("         AULA FINAL DA INTRODUÇÃO AO C          ");
+    puts("");
+    puts("#----------------------------------------------#");
+}
+
+const char* print(char texto[]){
+    printf("%s\n", texto);
+}
+
+int main(){
+    FuncVaziaApr();
+    print("Até que em fim um print de Python hehe");
+}
