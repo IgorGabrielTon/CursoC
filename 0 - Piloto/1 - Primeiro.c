249b15408296f2c4e3fb2be2873c0d2b826cd07d
Igor Gabriel <157933036+IgorGabrielTon@users.noreply.github.com>
2024-03-17 00:17:38 -0300

    Create 1 - Primeiro.c

---
+++ 0 - Piloto/1 - Primeiro.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  //Permite acesso a funções básicas e entrada e saída
+#include <stdlib.h> //Permite acesso a funções de controle (system por ex.)
+
+int main(){
+
+    printf("Olá Mundo Código C!\n");
+    printf("Meu primeiro app!");
+    system("pause");
+
+    return 0;
+}
+
+//gcc MeuCodigo.c -o MeuApp.exe    -> Para compilar!
+
+
